d1035769be350c8b58cffbe3bffd1c7d63340d22
Maxwell Mohrman <mwmohrma@ncsu.edu>
2021-02-02 21:16:33 -0500

    updates to cucumber/selenium delete recipe test robustness

--- CoffeeMaker/src/test/java/edu/ncsu/csc/CoffeeMaker/cucumber/web/DeleteRecipeWebStepDefs.java
+++ CoffeeMaker/src/test/java/edu/ncsu/csc/CoffeeMaker/cucumber/web/DeleteRecipeWebStepDefs.java
@@ -66,6 +66,48 @@ public class DeleteRecipeWebStepDefs extends CucumberTest {
         driver.findElement( By.linkText( "Home" ) ).click();
     }
 
+    @Given ( "^there are recipes called (.+) and (.+)$" )
+    public void insertRecipes ( final String name1, final String name2 ) {
+        DBUtils.resetDB( dataSource );
+        baseUrl = "http://localhost:8080";
+        driver.manage().timeouts().implicitlyWait( 10, TimeUnit.SECONDS );
+        driver.get( baseUrl + "" );
+
+        driver.findElement( By.linkText( "Add a Recipe" ) ).click();
+
+        driver.findElement( By.name( "name" ) ).clear();
+        driver.findElement( By.name( "name" ) ).sendKeys( name1 );
+        driver.findElement( By.name( "price" ) ).clear();
+        driver.findElement( By.name( "price" ) ).sendKeys( "1" );
+        driver.findElement( By.name( "coffee" ) ).clear();
+        driver.findElement( By.name( "coffee" ) ).sendKeys( "1" );
+        driver.findElement( By.name( "milk" ) ).clear();
+        driver.findElement( By.name( "milk" ) ).sendKeys( "1" );
+        driver.findElement( By.name( "sugar" ) ).clear();
+        driver.findElement( By.name( "sugar" ) ).sendKeys( "1" );
+        driver.findElement( By.name( "chocolate" ) ).clear();
+        driver.findElement( By.name( "chocolate" ) ).sendKeys( "1" );
+
+        driver.findElement( By.cssSelector( "input[type=\"submit\"]" ) ).click();
+
+        driver.findElement( By.name( "name" ) ).clear();
+        driver.findElement( By.name( "name" ) ).sendKeys( name2 );
+        driver.findElement( By.name( "price" ) ).clear();
+        driver.findElement( By.name( "price" ) ).sendKeys( "2" );
+        driver.findElement( By.name( "coffee" ) ).clear();
+        driver.findElement( By.name( "coffee" ) ).sendKeys( "1" );
+        driver.findElement( By.name( "milk" ) ).clear();
+        driver.findElement( By.name( "milk" ) ).sendKeys( "1" );
+        driver.findElement( By.name( "sugar" ) ).clear();
+        driver.findElement( By.name( "sugar" ) ).sendKeys( "1" );
+        driver.findElement( By.name( "chocolate" ) ).clear();
+        driver.findElement( By.name( "chocolate" ) ).sendKeys( "1" );
+
+        driver.findElement( By.cssSelector( "input[type=\"submit\"]" ) ).click();
+
+        driver.findElement( By.linkText( "Home" ) ).click();
+    }
+
     @When ( "^I delete the recipe with name (.+)$" )
     public void deleteRecipe ( final String recipeName ) {
 
@@ -75,8 +117,23 @@ public class DeleteRecipeWebStepDefs extends CucumberTest {
 
     }
 
+    @When ( "^I delete the recipes with delete all option$" )
+    public void deleteRecipes () {
+
+        driver.findElement( By.linkText( "Delete Recipe" ) ).click();
+        driver.findElement( By.cssSelector( "input[type=\"checkbox\"]" ) ).click();
+        driver.findElement( By.cssSelector( "input[type=\"submit\"]" ) ).click();
+
+    }
+
     @Then ( "^the recipe is deleted from the system$" )
     public void confirmDeleted () {
+        assertTextPresent( "Recipe deleted successfully", driver );
+        driver.findElement( By.linkText( "Home" ) ).click();
+    }
+
+    @Then ( "^they are deleted$" )
+    public void confirmDeletedAll () {
         assertTextPresent( "Recipe deleted successfully", driver );
         driver.findElement( By.linkText( "Home" ) ).click();
     }
